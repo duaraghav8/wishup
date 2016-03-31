2bfc4b28dd90bdc3135876ef06397a3ff8e05c3f
duaraghav8@gmail <duaraghav8@gmail.com>
2016-03-31 17:05:09 +0530

    Modified Cron.js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wishup
 Wishup Programming Assignment - as part of my internship application
+
+A simple To-Do list based in MEAN Stack with:
+  Geolocation & Timepicker Functionality
+  Automatic Emailing mechanism (15 minutes before an item's deadline)
+  Front end UI taken from http://www.todomvc.com/
--- app/cron/cron.js
+++ app/cron/cron.js
@@ -8,9 +8,12 @@ module.exports = {
       setTimeout, when timed out, spawn process with user info
       to send mail and save them from getting intercoursed
     */
-    var itemDeadline = new Date (item.deadline),
-      timeToLapse = itemDeadline.getTime () - (new Date ()).getTime ();   //this could cause problems if the server time differs from client time
+    var emailDue = new Date (item.deadline), timeToLapse = null;
 
+    emailDue.setMinutes (emailDue.getMinutes () - 15);
+    timeToLapse = emailDue.getTime () - (new Date ()).getTime ();   //this could cause problems if the server time differs from client time
+
+    if (emailDue < (new Date ())) { return; }
     setTimeout (function () {
       //spawn process to fire email reminder
       childProcess
